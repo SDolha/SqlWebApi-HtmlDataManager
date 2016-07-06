0922c30ce1308bccad0b77de59cfb5935d1805c8
Sorin Dolha <SDolha@Hotmail.com>
2016-07-06 19:08:45 +0300

    Initial commit

---
+++ DataManager/DataManager/Scripts/app.js
@@ -0,0 +1,15 @@
+﻿angular.module('MyOrganizationApp', [])
+    .controller('EmployeesCtrl', function ($scope, $http) {
+        var initialize = function () {
+            $http.get('/api/Employees').then(function (response) {
+                $scope.employees = response.data;
+            });
+            $scope.newEmployee = { FirstName: 'Test', LastName: 'Employee' };
+        };
+        initialize();
+        $scope.addNewEmployee = function () {
+            $http.post('/api/Employees', $scope.newEmployee).then(function () {
+                initialize();
+            });
+        }
+    });
---
+++ README.md
@@ -0,0 +1,2 @@
+# SqlWebApi-HtmlDataManager
+Simple data manager application leveraging HTML on top of ASP .NET WebApi and SQL Server database, developed with Visual Studio and as little code as possible.
